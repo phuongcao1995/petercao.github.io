38fba653db11d6c742ee8d408f790282db805e95
Laptop242\Peter <peter>
2024-09-05 13:47:36 +0700

    change to ASP.NET

---
+++ WebVideoImage/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebVideoImage.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
